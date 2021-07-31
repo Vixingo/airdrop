6c428690b5934eaf04567092f96f6aa3ee5a46a6
Md. Nazmu Khandoker <Nazmul.abbu@gmail.com>
2021-08-01 00:08:27 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Route, Link, HashRouter } from "react-router-dom";
+import Homepage from './pages/Homepage';
+
+function App() {
+  return (
+    <HashRouter>
+      <>
+      <Route exact path="/" component={Homepage} />
+      </>
+    </HashRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+function Homepage() {
+    return (
+        <>
+        <Navbar/>
+            <h1>hello Home</h1>
+        </>
+    )
+}
+
+export default Homepage
